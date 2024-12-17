c5bc3e28544289bbdb8f05c8b9a2ae1f1d2cd704
anandkprajapati123 <anandkprajapati123@gmail.com>
2024-12-17 10:06:05 +0530

    Add files via upload

---
+++ COMPLETE JS/Lecture5/functionpractice.js
@@ -0,0 +1,26 @@
+// Q.1
+// function countVowel(str){
+//     let count=0;
+//     for(const char of str){
+//         console.log(char);
+//         if(char==="a" ||    char==="e" || char==="i" || char==="o" || 
+//         char==="u"){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+//     return count;
+// }
+
+// Q.2
+const countVowel =(str)=>{
+    let count=0;
+    for(const char of str){
+        // console.log(char);
+        if(char==="a" || char==="e" || char==="i" || char==="o" || char==="u"){
+            count++;
+        }
+    }
+    // console.log(count);
+    return count;
+}
